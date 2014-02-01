f08cc8b12c2bc74fa67fd3a0acb3695bbbe90592
wsugarman <will.sugarman@gmail.com>
2014-02-01 09:40:43 -0500

    Added comments

--- FRC3157/src/edu/wpi/first/wpilibj/templates/InputData.java
+++ FRC3157/src/edu/wpi/first/wpilibj/templates/InputData.java
@@ -7,39 +7,107 @@
 package edu.wpi.first.wpilibj.templates;
 
 /**
- *
- * @author FRCUser
+ * User input used by roboThink
+ * @author Programming subteam
  */
 public class InputData {
-    // index 0 is X, index 1 is Y, index 2 is Z
+    
+    /**
+     * The coordinates for the left driver stick where
+     * index 0 is X, index 1 is Y, index 2 is Z
+     */
     public static double leftDriverStick[]={0,0,0};
+    
+    /**
+     * The coordinates for the right driver stick where
+     * index 0 is X, index 1 is Y, index 2 is Z
+     */
     public static double rightDriverStick[]={0,0,0};
+    
+    /**
+     * The coordinates for the co-driver stick where
+     * index 0 is X, index 1 is Y, index 2 is Z
+     */
     public static double coDriverStick[]={0,0,0};
     
+    /**
+     * Is the shooter button pressed?
+     */
     public static boolean shooterButtonPressed=false;
+    
+    /**
+     * Is the grabber button pressed?
+     */
     public static boolean grabberButttonPressed=false;    
     
+    /**
+     * Is the shooter extracted
+     */
     public static boolean bShooterExt=false;
+    
+    /**
+     * Is the shooter retracted?
+     */
     public static boolean bShooterRet=false;
   
+    /**
+     * The number of rotations performed by the right motor
+     */
     public static int leftMotorEncoderVal;
+    
+    /**
+     * The number of rotations performed by the right motor
+     */
     public static int rightMotorEncoderVal;
     
+    /**
+     * The time elapsed over the last left encoder interval
+     */
     public static double fLeftEncoderTime;
+    
+    /**
+     * The time elapsed over the last right encoder interval
+     */
     public static double fRightEncoderTime;
 
+    /**
+     * Are we driving straight?
+     */
     public static boolean bDriveStraightPressed = false;
     
+    /**
+     * Is automatic shooting and catching enabled?
+     */
     public static boolean bAutoShootAndCatch = false;
     
+    /**
+     * Are we shooting?
+     */
     public static boolean bManualShoot = false;
+    
+    /**
+     * Is the pin locked?
+     */
     public static boolean bManualPin = false;
     
+    /**
+     * Are we shooting manually?
+     */
     public static boolean bPower = false;
     
+    /**
+     * Are we turning left automatically (as opposed to manually turning?)
+     */
     public static boolean bAutoturnLeft = false;
+    
+    /**
+     * Are we turning right automatically (as opposed to manually turning?)
+     */
     public static boolean bAutoturnRight = false;
     
+    /**
+     * Sets all fields to 0 or false
+     */
     public static void reset() {
         for(int ii=0; ii<3; ++ii){
             leftDriverStick[ii] = 0;
