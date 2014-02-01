b9e185fb85f95d796fcdeca24d08e0a1692b0edd
FRCUser <FRCUser@10.35.27.180>
2014-02-01 09:42:49 -0500

    Added comments to RoboThink

--- FRC3157/src/edu/wpi/first/wpilibj/templates/RoboThink.java
+++ FRC3157/src/edu/wpi/first/wpilibj/templates/RoboThink.java
@@ -9,38 +9,56 @@ package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
- *
+ * processes inputs to create a more reliable output
  * @author FRCUser
  */
 public class RoboThink {
 
+    /*
+    these are steps to the catcAndShoot function
+    */
     public static final int kCATCH_WAITING = 0;
     public static final int kCATCH_EXTEND = 1;
     public static final int kCATCH_RETRACT = 2;
     public static final int kCATCH_SLEEP = 3;
+    /*
+    This one is the current step
+    */
     public static int dCatchStep = 0;
     
-    
+    /*
+    these define the directions for the motors and may need to be changed
+    */
     public static double leftMultiplier=-1*FRCConfig.kMAX_SHOOTER_POWER;
     public static double rightMultiplier=FRCConfig.kMAX_SHOOTER_POWER;
     
-    public int dFiringStep=0;
+    /**
+     * this is a general timer for RoboThink functions
+     */
     public Timer thinkTimer;
     
+    /*
+    RPM values calculated from the encoders
+    */
     public double fLeftMotorSpeed;
     public double fRightMotorSpeed;
     
+    /*
+    PID error values
+    */
     public double fLeftLastError = 0;
     public double fRightLastError = 0;
-    
     public double fLeftError = 0;
     public double fRightError = 0;
     
     public RoboThink(){
-        dFiringStep = 0;
         thinkTimer = new Timer();
     }
-    
+ 
+    /**
+     * Connects buttons to functions
+     * Takes in InputData and changes the OutputData
+     */
     public void processInputs(){
         if( InputData.leftDriverStick[1] > 0 ) {
             OutputData.leftMotorVal=leftMultiplier*(InputData.leftDriverStick[1]*InputData.leftDriverStick[1]);
@@ -92,7 +110,12 @@ public class RoboThink {
       ScreenOutput.clrLine( 1 );
       ScreenOutput.screenWrite("Right Motor Speed: " + fRightMotorSpeed, 1);
     }
-   
+   /**
+    * Automatically determines firing step
+    * first, while button is pressed, the firing pin is pulled
+    * next, while the button is not pressed, it does not fire and the pistons are retracted
+    * last, it recharges for the next launch
+    */
     public void fire(){
         if(InputData.shooterButtonPressed){
             OutputData.bPullPin=true;
@@ -109,13 +132,22 @@ public class RoboThink {
             ScreenOutput.screenWrite("Fire Step: Waiting (Charging Pstons)", 2);
         }
     }
+    /**
+     * Calculates the speed of the motors using encoders
+     * @param encoderCount number of times the encoder detects something
+     * @param encoderTime time since the last encoder reading
+     * @return gives the speed of the motor in RPM
+     */
     public double CalcMotorSpeed(int encoderCount, double encoderTime){
         double speed;
         speed = (double)encoderCount/(double)FRCConfig.kENCODER_PPR;
         speed/= ((double)encoderTime/60000);
         return speed;    
     }
-    
+    /**
+     * matches left motor speed to right motor speed
+     * uses PID values as defined in FRCConfig file
+     */
     public void steerStraightPID() {
         double expectPower = (OutputData.leftMotorVal*FRCConfig.kMAX_MOTOR_SPEED + OutputData.rightMotorVal*FRCConfig.kMAX_MOTOR_SPEED) / 2.0;
         
@@ -132,8 +164,21 @@ public class RoboThink {
         fRightLastError = currErrorRight;
         fLeftError += currErrorLeft;
         fRightError += currErrorRight;
+        if(OutputData.leftMotorVal>FRCConfig.kMAX_MOTOR_POWER){
+            OutputData.leftMotorVal = FRCConfig.kMAX_MOTOR_POWER;
+        }else if(OutputData.leftMotorVal<-1*FRCConfig.kMAX_MOTOR_POWER){
+            OutputData.leftMotorVal = -1*FRCConfig.kMAX_MOTOR_POWER;
+        }
+          if(OutputData.rightMotorVal>FRCConfig.kMAX_MOTOR_POWER){
+            OutputData.rightMotorVal = FRCConfig.kMAX_MOTOR_POWER;
+        }else if(OutputData.rightMotorVal<-1*FRCConfig.kMAX_MOTOR_POWER){
+            OutputData.rightMotorVal = -1*FRCConfig.kMAX_MOTOR_POWER;
+        }
     }
-    
+    /**
+     * matches left and right motor speeds
+     * uses percent error to determine change required in other value
+     */
     public void steerStraight(){
         double expectPowerLeft = OutputData.leftMotorVal*FRCConfig.kMAX_MOTOR_SPEED;
         double expectPowerRight = OutputData.rightMotorVal*FRCConfig.kMAX_MOTOR_SPEED;
@@ -159,10 +204,14 @@ public class RoboThink {
             }
         }
     }
+    /**
+     * magical function to throw, drive, and the catch the ball over the truss
+     * first, start timer and begins shooting while button is pressed
+     * second, wait for a predetermined amount of time, drives forward and resets the timer while button is pressed
+     * third, wait for another predetermined amount of time and stops moving
+     */
     public void catchAndShoot(){
         switch(dCatchStep){
-            case kCATCH_WAITING:
-                break;
             case kCATCH_EXTEND:
                 InputData.shooterButtonPressed = true;
                 thinkTimer.reset();
